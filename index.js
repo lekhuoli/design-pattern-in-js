c0fb9b79cbdedc1459e8d2230a239cc752b1db4a
Lekhnath Oli <lekhnath@fusemachines.com>
2023-11-21 09:16:04 +0545

    intro to design pattern

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 /* design patterns are common architectural approches
 
+ Solid principle 
+  a. Single responsibility Principle
+  b. Open-closed Principle
+  c. Liskov Substitutaion Principle
+  d. Interface Segregation Principle
+  e. Dependency Injection Principle
+
+
+Design patterns
+
+1. Creation Patterns
+  a. Builder 
+  b. Factories
+    i. Abstract Factory
+    ii. Factory Method
+  c. Prototype
+  d. Singleton
+
+2. Structural Patterns
+  a. Adapter
+  b. Bridge
+  c. Composite
+  d. Decorator
+  e. Facade
+  f. Flywheight
+  g. Proxy
+
+3. Behavior Patterns
+  a. Chain of Responsibility
+  b. Command
+  c. Interpreter
+  d. Iterator
+  e. Mediator
+  f. Memento
+  g. Observer
+  h. State
+  i. Strategy
+  j. Template Method
+  i. Visitor
+
+
 */
