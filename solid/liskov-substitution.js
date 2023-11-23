a0d690548a8e13153e1a80991df10c943de75e48
Lekhnath Oli <lekhnath@fusemachines.com>
2023-11-23 13:08:56 +0545

    Fix liskov substituion principle issue with seperation of class

--- solid/liskov-substitution.js
+++ solid/liskov-substitution.js
@@ -2,39 +2,75 @@
  * Objects of superclass should be replaced with objects of subclass 
    without affecting the correctness of the program
  */
-class Bird {
+// class Bird {
+//   fly() {
+//     console.log(`I can fly`);
+//   }
+// }
+
+// class Duck extends Bird {
+//   quack() {
+//     console.log("I can quack");
+//   }
+
+//   fly() {
+//     throw new Error("cann't fly");
+//   }
+// }
+
+// class Penguin extends Bird {
+//   swim() {
+//     console.log(`I can swim`);
+//   }
+// }
+
+// function makeBirdFly(bird) {
+//   bird.fly();
+// }
+
+// const duck = new Duck();
+// const penguin = new Penguin();
+
+// makeBirdFly(duck);
+// makeBirdFly(penguin);
+
+/**
+On this above program it contradicts the liskov substitution principle when we try to implement
+makeBirdFly with duck becasue it try to remove the exiting functaonality of base class
+
+To resolve this issue make sepeate base classes for swim and fly birds
+*/
+
+class FlyingBird {
   fly() {
-    console.log(`I can fly`);
+    console.log("I can fly");
   }
 }
 
-class Duck extends Bird {
+class SwimmingBird {
+  swim() {
+    console.log("I can swim");
+  }
+}
+
+class Duck extends FlyingBird {
   quack() {
     console.log("I can quack");
   }
-
-  fly() {
-    throw new Error("cann't fly");
-  }
 }
 
-class Penguin extends Bird {
-  swim() {
-    console.log(`I can swim`);
-  }
-}
+class Penguin extends SwimmingBird {}
 
-function makeBirdFly(bird) {
+function makeFlyingBirdFly(bird) {
   bird.fly();
+}
+
+function makeSwimmingBirdSwim(bird) {
+  bird.swim();
 }
 
 const duck = new Duck();
 const penguin = new Penguin();
 
-makeBirdFly(duck);
-makeBirdFly(penguin);
-
-/**
-on this above program it contradicts the liskov substitution principle when we try to implement
-makeBirdFly with duck
-*/
+makeFlyingBirdFly(duck);
+makeSwimmingBirdSwim(penguin);
