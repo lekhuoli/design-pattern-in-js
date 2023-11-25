dd50029024e8b39a1f510965fa156bf6726a5ff8
Lekhnath Oli <lekhnath@fusemachines.com>
2023-11-25 14:20:37 +0545

    Feat: Research high level module

--- solid/dependency-inversion.js
+++ solid/dependency-inversion.js
@@ -25,7 +25,19 @@ class Relationships {
   }
 }
 
-
+//HIGH-LEVEL MODULE
+// Find relation shipe between the person
+class Research {
+  constructor(relationships) {
+    //find all children of John
+    const relations = relationships.data;
+    for (let rel of relations.filter(
+      (r) => r.from.name === "John" && r.type === RELATIONSHIP.parent
+    )) {
+      console.log(`John  has child name ${rel.to.name}`);
+    }
+  }
+}
 const parent = new Person("John");
 const child1 = new Person("Chris");
 const child2 = new Person("Matt");
@@ -34,3 +46,9 @@ const rels = new Relationships();
 rels.addParentAndChild(parent, child1);
 rels.addParentAndChild(parent, child2);
 
+new Research(rels);
+
+
+// On the above  Reserach HIGH-LEVEL MODULE is dependent on Relationships LOW-LEVEL MODULE
+//which violates the Dependency Inversion Principle 
+// to reolsve this
