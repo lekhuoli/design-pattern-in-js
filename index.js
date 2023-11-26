20e51ce3b8c45470a38091dad21cfff3757f21dc
Lekhnath Oli <lekhnath@fusemachines.com>
2023-11-26 10:56:59 +0545

    Feat: add more details on design pattern type

--- index.js
+++ index.js
@@ -9,8 +9,12 @@
 
 
 Design patterns
-
+GAMAA CATEGORIZATION 
 1. Creation Patterns
+- Deal with creation(constructor) of objects
+- Explicit(constructor) vs Implicit(DI,reflection etc)
+- Wholesale(single statment) vs Piecewise (step-by-step)
+
   a. Builder 
   b. Factories
     i. Abstract Factory
@@ -19,6 +23,10 @@ Design patterns
   d. Singleton
 
 2. Structural Patterns
+- Concerned with structure (eg. class members)
+- Many patterns are wrappers that mimic the underlying class inteface
+- Stress the importance of good API Design 
+
   a. Adapter
   b. Bridge
   c. Composite
@@ -28,6 +36,8 @@ Design patterns
   g. Proxy
 
 3. Behavior Patterns
+- They are all different, no central theme
+
   a. Chain of Responsibility
   b. Command
   c. Interpreter
